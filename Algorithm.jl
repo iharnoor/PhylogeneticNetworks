12c48d18d2b614d5c0d31dc4ae381cebebdc0130
Naman Kanwar <nkanwar1@student.gsu.edu>
2017-12-27 20:55:02 -0500

    update

--- Algorithm.jl
+++ Algorithm.jl
@@ -20,37 +20,61 @@ function computeSN(T,L,x,y)
     X= [x]
     Z= [y]
 	computed= []
+	L1= []
+	L1= copy(L)
     while(!isempty(Z))
 		z=Z[1]
+		# println("L= ", string(L))
+		println("Z= ", string(Z))
         for a in X
-			filter!(x -> x!=a,L)
-			filter!(x -> x!=z,L)
+			L= copy(L1)
+			for p in X
+				filter!(x -> x!=p,L)
+			end
+			for p in Z
+				filter!(x -> x!=p,L)
+			end
+			# println("X= ", string(X))
+			# println("a= ", string(a))
+			# println("Z= ", string(Z))
+			# println("z= ", string(z))
+			# println("L1= ", string(L1))
+
             for c in L
             	temp=[a,c,z]
             	temp2=[z,c,a]
             	if(temp in T && !(temp in computed)) #pseudo code
             		#Z=Z U {c}
-            		append!(Z,c)
+					append!(Z,c)
+					Z = unique(Z)
+
+				#	println("Z= ", string(Z))
 					append!(computed, temp)
             	end
             	if(temp2 in T && !(temp2 in computed))
             		append!(Z,c)
+					Z = unique(Z)
 					append!(computed, temp2)
             	end
             	#or contains!(T,temp2))
+				println("Z= ", string(Z))
             end
         end
         h =Z[1]
         append!(X,h)
         splice!(Z,1)
+		println("Z= ", string(Z))
+		println("--------------")
+
+		#println("Z= ", string(Z))
     end
     return X
 end
 
-T= [[1,2,3], [1,2,4], [1,2,5], [2,3,4], [2,3,4], [2,3,5], [3,4,5], [1,3,4],
-[1,3,4], [1,3,5], [1,4,5], [2,4,5]]
+T= [[1,2,3], [1,2,4], [1,2,5], [2,3,4], [3,4,2], [2,3,5], [3,4,5], [1,3,4],
+[3,4,1], [1,3,5], [1,4,5], [2,4,5]]
 
 L= [1,2,3,4,5]
 
-N= computeSN(T, L,1,4)
+N=computeSN(T, L,1,3)
 print(N)
