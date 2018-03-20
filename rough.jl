f43f6702ca8fef3cf677f7fddba61ce8a0355620
Naman Kanwar <nkanwar1@student.gsu.edu>
2018-03-19 21:54:35 -0400

    Adjacency List
    Add and remove methods for Adjacency List

--- rough.jl
+++ rough.jl
@@ -1,4 +1,4 @@
-using DataStructures
+# using DataStructures
 T= [[1,2,3], [1,2,4], [1,2,5], [2,3,4], [3,4,2], [2,3,5], [3,4,5], [1,3,4],
 [3,4,1], [1,3,5], [1,4,5], [2,4,5],[2,1,3]]
 # L= [1,2,3,4,5]
@@ -23,4 +23,59 @@ for i in T
         push!(alpha,ds)
     end
 end
-print(unique(alpha))
+# print(unique(alpha))
+
+# value=6
+# append!(adjList[x],value) # Appending a value
+# filter!(x -> x!=2,adjList[x]) # Removing a value
+# print(adjList)
+# addEdge(adjList,x,4)
+
+
+function addEdge(dictionary, key, value)
+    # adjList= Dict(-1 => [1,2,3])
+    # x=-1
+    # dictionary[key]= value
+    # value=6
+    if(haskey(dictionary,key))
+        append!(dictionary[key],value) # Appending a value
+    else
+         dictionary[key]=[value]
+    end
+    if(haskey(dictionary,value))
+        append!(dictionary[value],key) # Appending a value
+    else
+         dictionary[value]=[key]
+    end
+
+    # append!(dictionary[value],key) # Appending a value
+    print(dictionary)
+end
+function removeEdge(dictionary, key, value)
+    if(haskey(dictionary,key))
+        # append!(dictionary[key],value)
+        filter!(x -> x!=value,dictionary[key])
+        # if(dictionary[key]== Int64[])
+        #  delete!(dictionary,key)
+        # end
+    end
+    if(haskey(dictionary,value))
+        # append!(dictionary[key],value)
+        filter!(x -> x!=key,dictionary[value])
+        # if(dictionary[value]== Int64[])
+        #     delete!(dictionary,value)
+        # end
+    end
+
+
+    # filter!(x -> x!=value,dictionary[key]) # Removing a value
+    print(dictionary)
+end
+
+adjList= Dict(-1 => [1,2,3])
+# x=-1
+# adjList[-1]= [-4,2,-3]
+# adjList[2]=[6,9]
+print(adjList)
+addEdge(adjList,-1,4)
+removeEdge(adjList,-1,4)
